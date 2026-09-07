d04c8462aad3e83f3616b9cecc3fc62b5cd131c6
jasarsyed04-glitch <jasarsyed04@gmail.com>
2026-09-07 12:15:46 +0530

    Change redirect delay to 3000ms
    Increased delay before redirecting to invitation.html.

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       setTimeout(() => {
         window.location.href = "invitation.html";
-      }, 900);
+      }, 3000);
     };
 
     envelope.addEventListener("click", open);
